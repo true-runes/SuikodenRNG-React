f12c19b795afa09970a5f450c168249054e9bb6d
Alexandr Kolesnik <alexandr.kolesnik.51@my.csun.edu>
2018-03-12 03:14:55 -0700

    Switched to HashRouter for gh-pages support.

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'semantic-ui-css/semantic.min.css';
 import 'react-virtualized/styles.css';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <App/>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
