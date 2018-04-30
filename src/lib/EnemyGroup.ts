b013df01d6078c5af3334d40b4983de779cc94ba
Alexandr Kolesnik <alexandr.kolesnik.92@gmail.com>
2018-04-30 00:10:34 -0700

    Fixed bug in drop calculation that would give inaccurate results.

--- src/lib/EnemyGroup.ts
+++ src/lib/EnemyGroup.ts
@@ -16,24 +16,25 @@ export default class EnemyGroup {
   public calculateDrops(rng: RNG, iterations: number) {
     const drops: CalculatedDrop[] = [];
     for (let i = 0; i < iterations; i++) {
-      const drop = this.calculateDrop(rng);
+      const drop = this.calculateDrop(rng.clone());
       drops.push({ rng: rng.getRNG(), drop });
       rng.next();
     }
     return drops;
   }
 
+  // This function advances RNG
   private calculateDrop(rng: RNG): string {
     for (const enemy of this.enemies) {
-        let r2 = rng.getNext().rng2;
-        const dropIndex = r2 % 3;
-        if (dropIndex < enemy.drops.length) {
-          const dropRate = enemy.drops[dropIndex].rate;
-          r2 = rng.getNext(2).rng2;
-          if (r2 % 100 < dropRate) {
-            return enemy.drops[dropIndex].item;
-          }
+      let r2 = rng.next().getRNG2();
+      const dropIndex = r2 % 3;
+      if (dropIndex < enemy.drops.length) {
+        const dropRate = enemy.drops[dropIndex].rate;
+        r2 = rng.next().getRNG2();
+        if (r2 % 100 < dropRate) {
+          return enemy.drops[dropIndex].item;
         }
+      }
     }
     return '';
   }
